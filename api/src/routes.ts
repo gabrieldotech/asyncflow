467a51e13ca7022d29581da4afbd820b36cc9fd5
Gabriel Macena Ramos <gabrieldotech@gmail.com>
2026-01-22 15:23:27 -0300

    feat: add zod validation for video conversion request

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,18 +1,20 @@
 import { FastifyInstance } from "fastify";
 import { Channel } from "amqplib";
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
+import { z } from 'zod';
 
 export async function videoRoutes(
   app: FastifyInstance,
   { channel }: { channel: Channel },
 ) {
   const QUEUE_NAME = "pedidos_video";
-  app.post("/convert", async (request, reply) => {
-    const { videoName } = request.body as any;
 
-    if (!videoName) {
-      return reply.status(400).send({ error: "videoName is required" });
-    }
+  const convertVideoSchema = z.object({
+    videoName: z.string({ message: "videoName must be a string" })
+      .min(1, "videoName is required and cannot be empty"),
+  });
+  app.post("/convert", async (request, reply) => {
+    const { videoName } = convertVideoSchema.parse(request.body)
 
     const message = {
       id: uuidv4(),
@@ -21,7 +23,6 @@ export async function videoRoutes(
     };
 
     // publish the message to the queue
-
     channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(message)), {
       persistent: true,
     });
